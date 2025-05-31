cf0a6917b3ed35c5a6c5f6202e352a38ab1a8fbd
Charbel <charbelabou2000@outlook.com>
2025-05-31 09:34:52 +0300

    Add ProductService

---
+++ ProdcutService/Domain/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public float Price { get; set; }
+}
---
+++ ProdcutService/Persistance/ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(p => p.Id);
+            modelBuilder.Entity<Product>().Property(p => p.Name).IsRequired();
+            modelBuilder.Entity<Product>().Property(p => p.Price).IsRequired();
+        }
+    }
+}
